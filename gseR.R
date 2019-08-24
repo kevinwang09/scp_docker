49aad9ef98c95926f93448d17ca9810d89fbe3cd
kevinwang09 <kevin.wang@sydney.edu.au>
2019-08-24 20:45:35 +1000

    unzip bug fix 2

--- gseR.R
+++ gseR.R
@@ -9,7 +9,7 @@ gce_global_zone(zone)
 # gce_list_zones(project)
 # gce_list_machinetype()$items
 
-(tag = gce_tag_container("scp_docker:687828a"))
+(tag = gce_tag_container("scp_docker:4ae8803"))
 
 vm <- gce_vm(template = "rstudio", 
              name = "singlecellplus1", 
