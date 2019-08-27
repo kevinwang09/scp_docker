647c179e7b1738151dcea71bbe05a749de59ef40
kevinwang09 <kevin.wang@sydney.edu.au>
2019-08-27 14:29:41 +1000

    changed names and omg.R

--- omg.R
+++ omg.R
@@ -1,4 +1,14 @@
 ## If things goes wrong, 
-## the admin of the workshop will git clone https://github.com/SydneyBioX/SingleCellPlus into /home/tmp/
+## the admin of the workshop will 
+#
+# sudo rm -rf /home/tmp/*
+# cd /home/tmp/
+# sudo git clone https://github.com/SydneyBioX/SingleCellPlus 
+# sudo cp -r /home/tmp/SingleCellPlus/* ./
+# sudo rm -rf SingleCellPlus
 ## and ask all users to run this script
 system(paste0("cp -r /home/tmp/* ", getwd()))
+
+devtools::install_github("IndrajeetPatil/kittyR")
+set.seed(100)
+kittyR::kittyR(meow = FALSE)
